8b7f5fd0367c5147418de17ff34be92c7c2e2388
EkaterinaLegalovaCH <legalovaev83@gmail.com>
2023-06-22 10:05:39 +0200

    applied promotions to list of products, changed buy function, to count according discount

--- products.py
+++ products.py
@@ -1,13 +1,18 @@
 """
 create a class Product
-with parametrs: name, price and quantity,
+with parameters: name, price and quantity,
 and methods such as set quantity, show info about
 product, add product, remove product,
 and so on
 """
+
+
 class Product:
 
-    def __init__(self, name, price, quantity):
+    def __init__(self, name, price, quantity, promotion):
+        self.promotion = promotion
+        self.order_quantity = 0
+        self.promotion = None
         try:
             if name == "":
                 raise ValueError("Name of product can't be an empty string")
@@ -30,6 +35,7 @@ class Product:
             print("Error: ", str(e))
             self.quantity = 0
         self.active = True
+        self.promotion = None
 
     def get_quantity(self):
         if self.quantity == 0:
@@ -57,12 +63,15 @@ class Product:
     def show(self):
         print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
 
+    def set_promotion(self, promotion):
+        self.promotion = promotion
 
-    def buy(self, quantity):
+    def buy(self, order_quantity):
+        self.order_quantity = order_quantity
         try:
-            if self.quantity >= quantity or quantity <= 0 or self.active is False:
-                self.quantity -= quantity
-                purchase = quantity * self.price
+            if self.quantity >= self.order_quantity or self.order_quantity <= 0 or self.active is False:
+                self.quantity -= self.order_quantity
+                purchase = self.order_quantity * self.price - self.promotion.apply_promotion(self, self.order_quantity)
                 if self.quantity == 0:
                     self.deactivate()
                 return purchase
@@ -72,24 +81,29 @@ class Product:
             print(e)
             return str(e)
 
+
 class NonStockedProduct(Product):
-    def __init__(self, name, price):
-        super().__init__(name, price, quantity=0)
+    def __init__(self, name, price, promotion):
+        super().__init__(name, price, quantity=0, promotion=promotion)
+        self.promotion = promotion
 
     def buy(self, quantity):
+        if self.promotion is not None:
+            purchase = quantity * self.price - self.promotion.apply_promotion(self, quantity)
+        else:
             purchase = quantity * self.price
-            return purchase
+        return purchase
 
 
 class LimitedProduct(Product):
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
+    def __init__(self, name, price, quantity, promotion):
+        super().__init__(name, price, quantity, promotion)
 
     def buy(self, quantity):
         try:
             if self.quantity >= quantity or quantity <= 0 or self.active is False:
                 self.quantity -= quantity
-                purchase = quantity * self.price
+                purchase = quantity * self.price - self.promotion
                 if self.quantity == 0:
                     self.deactivate()
                 return purchase
@@ -98,8 +112,3 @@ class LimitedProduct(Product):
         except Exception as e:
             print(e)
             return str(e)
-
-
-
-
-
--- promotions.py
+++ promotions.py
@@ -1,14 +1,15 @@
 
 """
-created class Promotion
+created class Promotion, with 3 subclasses of different promotions,
+return amount of discount
 """
 from abc import ABC, abstractmethod
 
+
 class Promotion(ABC):
-    def __init__(self, name):
+    def __init__(self, name, percent=None):
         self.name = name
-
-
+        self.percent = percent
 
     @abstractmethod
     def apply_promotion(self, product):
@@ -19,12 +20,33 @@ class SecondHalfPrice(Promotion):
     def __init__(self, name):
         super().__init__(name)
 
-    def apply_promotion(self, product):
-        print(product)
-        if product.quantity >= self.ordered_quantity:
-            discounted_amount = (self.ordered_quantity // 2) * (product.price / 2)
+    def apply_promotion(self, product, quantity):
+        if quantity > 1:
+            discounted_amount = (product.price / 2) * (int(quantity / 2))
         else:
             discounted_amount = 0
+
         return discounted_amount
 
 
+class ThirdOneFree(Promotion):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product, quantity):
+        if quantity > 1:
+            discounted_amount = product.price * (int(quantity / 3))
+        else:
+            discounted_amount = 0
+
+        return discounted_amount
+
+
+class PercentDiscount(Promotion):
+    def __init__(self, name, percent):
+        super().__init__(name)
+        self.percent = percent
+
+    def apply_promotion(self, product, quantity):
+        discounted_amount = (product.price * self.percent / 100) * quantity
+        return discounted_amount
